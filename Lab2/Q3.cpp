792c4c9b2a818153ce56a97ac0c383b86b6b24cd
Ahmed-prog70 <rashdiahmed03@outlook.com>
2025-09-01 21:39:28 +0500

    Update Q3.cpp

--- Lab2/Q3.cpp
+++ Lab2/Q3.cpp
@@ -2,32 +2,67 @@
 using namespace std;
 
 int main() {
-    int students, input;
-    cout<< "Enter number of students: ";
-    cin >> students;
-    int **arr = new int*[students]{0};
-    int courses[students];
-    for (int i = 0; i < students; i++) {
-        cout << "Enter number of courses for student: " << i + 1 << " : ";
-        cin >> courses[i];
-        arr[i] = new int[courses[i]]; }
-    for (int i = 0; i < students; i++) {
-        for (int j = 0; j < courses[i]; j++) {
-        	while (true){
-	        	cout << "Enter course " << j+1 << " marks for student "<< i+1<< ": ";
-	            cin >> input;
-				if ((input >= 0) && (input <= 100)) {
-					arr[i][j] = input;
-					break; }    		
-				else {
-					cout << "Marks should be between 0 to 100.\n"; } } }
-        cout << endl; }
-    for (int i=0; i<students; i++) {
-    	for(int j=0; j<courses[i]; j++) {
-    		cout<< "Student " << i+1 << " marks for course " << j+1 << " is: "<< arr[i][j] << "\n"; }
-		cout<<"\n"; }
-    for (int i = 0; i < students; i++) {
-        delete[] arr[i]; }
-    delete[] arr; 
+    int studentNum;
+    cout << "Enter number of students: ";
+    cin >> studentNum;
+    
+    while (studentNum <= 0) {
+        cout << "Invalid Input. ";
+        cin >> studentNum;
+    }
+    int* numCourses = new int[studentNum];
+    double** studentMarks = new double*[studentNum];
+    for (int i = 0; i < studentNum; i++) {
+         cout << "\nStudent " << (i + 1) << ":\n";
+         cout << "Enter number of courses: ";
+         cin >> numCourses[i];
+    while (numCourses[i] <= 0) {
+            cout << "Invalid input ";
+            cin >> numCourses[i];
+        }
+        
+        studentMarks[i] = new double[numCourses[i]];
+        
+cout << "Enter marks for " << numCourses[i] << " courses:\n";
+    for (int j = 0; j < numCourses[i]; j++) {
+         cout << "Course " << (j + 1) << ": ";
+         cin >> studentMarks[i][j];
+            
+            while (studentMarks[i][j] < 0 || studentMarks[i][j] > 100) {
+                cout << "Marks can only be between 0 and 100. ";
+                cin >> studentMarks[i][j];
+            }
+        }
+    }
+    
+    cout << "Report card\n";
+
+    for (int i = 0; i < studentNum; i++) {
+        double sum = 0.0;
+        
+        for (int j = 0; j < numCourses[i]; j++) {
+            sum += studentMarks[i][j];
+        }
+        
+        double average = (numCourses[i] > 0) ? sum / numCourses[i] : 0.0;
+        
+    cout << "Student " << (i + 1) << ": ";
+     cout << numCourses[i] << " courses, ";
+     cout << "Average = " << average;
+     cout << " Marks: ";
+        
+  for (int j = 0; j < numCourses[i]; j++) {
+            cout << studentMarks[i][j];
+            if (j < numCourses[i] - 1) cout << ", ";
+        }
+           cout << endl;
+    }
+    
+    for (int i = 0; i < studentNum; i++) {
+        delete[] studentMarks[i];
+    }
+    delete[] studentMarks;
+    delete[] numCourses;
+    
     return 0;
 }
