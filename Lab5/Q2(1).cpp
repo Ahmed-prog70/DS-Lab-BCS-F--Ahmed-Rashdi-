543f03b9b5339b8ee852ace4dfde698855dd4c69
Ahmed-prog70 <rashdiahmed03@outlook.com>
2025-09-19 12:09:48 +0500

    Create Q2(1).cpp

---
+++ Lab5/Q2(1).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void printnum(int n) {
+	if (n==1) {
+		cout<<1<<endl;
+		return ;
+	}
+	else {
+		cout<<n<<endl;
+		printnum(n-1);
+	}
+}
+int main() {
+	int n;
+	cout << "Enter number: ";
+	cin>>n;
+	printnum(n);
+}
